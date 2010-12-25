9cc0f4ba2bc2f1f0ba300634da3dff18ce49f195
sujan shrestha <sunnysujan@gmail.com>
2010-12-25 21:55:17 +0000

    adding two numbers

---
+++ src/example2.c
@@ -0,0 +1,12 @@
+/* Program ADDITION */
+#include <stdio.h>
+int main()
+{
+  int number;
+  float amount;
+  number = 100;
+  amount = 30.75+ 75.35;
+  printf("%d\n", number);
+  printf("%5.2f\n", amount);
+  return 0;
+}
