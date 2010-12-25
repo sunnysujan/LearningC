39cfd8c8397e3a23a852accc4a07e8b7a4a1ceb1
sujan shrestha <sunnysujan@gmail.com>
2010-12-25 19:24:20 +0000

    hello world file added

---
+++ src/HelloWorld.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : LearningC.c
+ Author      : sujan shrestha
+ Version     :
+ Copyright   : copyleft @ suJan 2010
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
